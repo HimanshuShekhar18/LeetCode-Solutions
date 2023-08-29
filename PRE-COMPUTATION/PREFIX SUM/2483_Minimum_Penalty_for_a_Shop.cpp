4f632c95a67cb4b51fb7fb81c4ec9ba3e4535e28
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-08-29 23:58:39 +0530

    Update 2483_Minimum_Penalty_for_a_Shop.cpp

--- PRE-COMPUTATION/PREFIX SUM/2483_Minimum_Penalty_for_a_Shop.cpp
+++ PRE-COMPUTATION/PREFIX SUM/2483_Minimum_Penalty_for_a_Shop.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
 
+    /* APPROACH:
+    standing at any index i;
+     we will check the no. of 'N' present in [0, i)  and the no. of 'Y' present in [i,n];
+     TC: O(n)
+     SC: O(1) 
+    */ 
+    int bestClosingTime(string customers) {
+        int currpenality = count(customers.begin(),customers.end(),'Y') ;
+        int minpenality = currpenality;
+        int indexstop = 0;
+        for(int i = 0; i<customers.size(); i++){
+            if(customers[i]=='Y'){
+                currpenality--;
+            }
+            if(customers[i]=='N') {
+                currpenality++;
+            }
+            if(currpenality<minpenality){
+                indexstop=i+1;
+                minpenality = currpenality;
+            }
+        }
+        return indexstop;
+    }
+};
