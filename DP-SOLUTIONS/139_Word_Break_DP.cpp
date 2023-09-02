d1ecac88fd9ccd34969fa273aba26eba41f6ec89
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-03 03:11:39 +0530

    Update 139_Word_Break_DP.cpp

--- DP-SOLUTIONS/139_Word_Break_DP.cpp
+++ DP-SOLUTIONS/139_Word_Break_DP.cpp
@@ -32,3 +32,41 @@ public:
     }
 };
 
+
+// <---------------------------------------------------O(n^2)----------------------------------------------------------------------------->
+
+class Solution {
+public:
+    /*
+    TC: O(n^2) 
+    SC: O(s.length*wordDict.length)
+    */
+    int n;
+    unordered_set<string> words;
+    int dp[301];
+    bool solve(int ind, string &s){
+        if(ind==n){
+            return true;
+        }
+
+        if(dp[ind]!=-1) return dp[ind];
+
+        if(words.find(s.substr(ind,n-ind)) != words.end()) return true;
+        string temp = "";
+        for(int i = ind; i<n; i++){
+            temp.push_back(s[i]);
+            if(words.find(temp)!=words.end() && solve(i+1,s)) return dp[ind]=true;
+        }
+        return dp[ind]=false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n = s.length();
+        memset(dp,-1,sizeof(dp));
+        words.insert(wordDict.begin(), wordDict.end());  // for comparison
+        return solve(0,s);
+    }
+};
+
+
+
