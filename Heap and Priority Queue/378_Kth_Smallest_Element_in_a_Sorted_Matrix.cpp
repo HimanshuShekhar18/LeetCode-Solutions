a72f1ee1edfa6e57fbea10e3af1d148c9a186b1f
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-02 15:03:00 +0530

    Update 378_Kth_Smallest_Element_in_a_Sorted_Matrix.cpp

--- Heap and Priority Queue/378_Kth_Smallest_Element_in_a_Sorted_Matrix.cpp
+++ Heap and Priority Queue/378_Kth_Smallest_Element_in_a_Sorted_Matrix.cpp
@@ -47,3 +47,37 @@ public:
     }
 };
 
+
+// <--------------------------------------------------------------------------Binary Search ----------------------------------------------------->
+
+class Solution {
+public:
+
+    // BINARY SEARCH
+    // TC: O(n*log(n))
+    // SC: O(1)
+
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+
+        int low = matrix[0][0];
+        int high = matrix[n-1][n-1];
+
+        while(low<=high){   
+            int mid = high-(high-low)/2;
+            int count = 0;
+
+            for(int i = 0; i<n; i++){
+                count+=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+            }
+            if(count<k){
+                low = mid+1;
+            }
+            else if(count>=k){    // changes done here
+                high=mid-1;
+            }
+        }    
+        return low;   // changes done here 
+    }
+};
+
