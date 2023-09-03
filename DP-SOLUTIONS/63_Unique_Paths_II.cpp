9d8ea5c38d27f42eb15320f1b70e13a3c5ffd7a2
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-03 13:24:49 +0530

    Update 63_Unique_Paths_II.cpp

--- DP-SOLUTIONS/63_Unique_Paths_II.cpp
+++ DP-SOLUTIONS/63_Unique_Paths_II.cpp
@@ -1,3 +1,54 @@
+// RECURSION + MEMOIZATION
+
+class Solution {
+public:
+
+int m,n;
+
+int dp[102][102];
+
+    int solve(int row, int col, vector<vector<int>> &obstacleGrid){
+
+        if(obstacleGrid[row][col]==1){
+            return 0;
+        }
+        if(row==m-1 && col==n-1){     // mistake was done here && honga na || ki jagaah
+            return 1;
+        }
+
+        if(dp[row][col]!=-1){
+            return dp[row][col];
+        }
+
+        int ways=0;
+
+        if(row<m-1){
+
+            ways+=solve(row+1,col,obstacleGrid);
+        }
+
+        if(col<n-1){
+            ways+=solve(row,col+1,obstacleGrid);
+        }
+
+        return dp[row][col]=ways;
+
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        m=obstacleGrid.size();
+        n=obstacleGrid[0].size();
+        memset(dp,-1,sizeof(dp));
+        if(obstacleGrid[0][0]==1) return 0;
+        if(obstacleGrid[m-1][n-1]==1) return 0;
+        return solve(0,0,obstacleGrid);
+        
+    }
+};
+
+
+// <--------------------------------------------------------------------TABULATION---------------------------------------------------------------------------->
+
+
 class Solution {
 public:
 
