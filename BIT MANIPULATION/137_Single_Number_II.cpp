5cf7e2ffd35bed731d92e3a4bff946720100bb10
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-04 08:29:54 +0530

    Update 137_Single_Number_II.cpp

--- BIT MANIPULATION/137_Single_Number_II.cpp
+++ BIT MANIPULATION/137_Single_Number_II.cpp
@@ -36,3 +36,38 @@ count%3 at each i'th bit position = --> =  1 1 1  = 7 --> ans
 };
 
 
+
+// <-------------------------------------------------------------------------------------------------------------------------------->
+
+
+
+
+class Solution {
+public:
+    // SORTING
+    // TC: O(n*log(n))
+    // SC: O(1)
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    
+        int n = nums.size();
+
+        if(n==1) return nums[0];
+
+        if(nums[0]!=nums[1]) return nums[0];
+
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        for(int i = 1; i<n-1; i++){
+            if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) return nums[i];
+        }
+        
+        return 0;
+
+    }
+};
+
+
+
+
+
