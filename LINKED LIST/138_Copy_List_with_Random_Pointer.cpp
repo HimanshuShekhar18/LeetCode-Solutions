29a7667a52d0b5578504236cd85d607dd9e2f169
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-06 13:27:29 +0530

    Update 138_Copy_List_with_Random_Pointer.cpp

--- LINKED LIST/138_Copy_List_with_Random_Pointer.cpp
+++ LINKED LIST/138_Copy_List_with_Random_Pointer.cpp
@@ -56,3 +56,71 @@ public:
     }
 };
 
+
+
+// <---------------------------------------------------------- SC: O(1) and Fast ------------------------------------------------------------------->
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    // TC: O(n)
+    // SC: ans space = O(n) but extra space O(1), hence SC: O(1) (as in Space Complexity we consider extra space not the ans space)
+    Node* copyRandomList(Node* head) {
+        if(head==NULL) return NULL;
+        Node* curr = head;
+
+        // 1) Insert the new nodes in between the Original Linked List
+        while(curr!=NULL){
+            // A --> B
+            Node* node = new Node(curr->val);  // x
+            Node* currNext = curr->next;     // B
+            curr->next = node;      // A --> x
+            node->next = currNext;  // A --> x --> B
+            curr = currNext;   // curr now at B
+        }
+        
+        // 2) Deep Copy of random pointers
+        curr = head;
+        while(curr!=NULL && curr->next!=NULL){
+            if(curr->random==NULL){
+                curr->next->random = NULL;
+            } 
+            else {
+                curr->next->random = curr->random->next;
+            }
+            curr=curr->next->next;      // check must be done for NULL access violation    
+        }
+
+        // 3) Separation the copied Linked List and Original Linked List
+        curr = head;
+        Node* newHead = head->next;
+        Node* newcurr = newHead;
+
+        while(curr!=NULL && newcurr!=NULL ){
+            // first we will separate curr from newHead
+            curr->next = curr->next == NULL? NULL: curr->next->next;  
+            curr=curr->next;
+            // then after that we will separate newHead from curr
+            newcurr->next = newcurr->next == NULL? NULL: newcurr->next->next;  
+            newcurr=newcurr->next;       
+        }
+        return newHead;
+    }
+};
+
+
+
