577d661e287119d7ad09c725cc205f0335c072e4
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-07 02:18:13 +0530

    Create 692_Top_K_Frequent_Words.cpp

---
+++ Heap and Priority Queue/692_Top_K_Frequent_Words.cpp
@@ -0,0 +1,36 @@
+class Compare {
+    public:
+       bool operator()(pair<int,string> a, pair<int,string> b){
+           if(a.first<b.first){
+               return true;
+           }
+           else if(a.first==b.first) return a.second>b.second;
+           else return false;
+      }
+};
+class Solution {
+public:
+    // Priority Queue
+    // TC: O(words.length())+O(n)+O(k)
+    // SC: O(n)+O(n)
+    vector<string> topKFrequent(vector<string>& words, int k) {
+
+        vector<string> ans;
+
+        unordered_map<string,int>mp;
+
+        for(auto word: words){
+            mp[word]++;
+        }
+        priority_queue<pair<int,string>,vector<pair<int,string>>,Compare> pq;
+        for(auto m: mp){
+            pq.push({m.second,m.first});
+        }
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
