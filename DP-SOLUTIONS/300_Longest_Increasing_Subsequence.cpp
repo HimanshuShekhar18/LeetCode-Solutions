d085e20af4f2850526e51bdc35c396b4c358e04b
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-10 19:27:54 +0530

    Update 300_Longest_Increasing_Subsequence.cpp

--- DP-SOLUTIONS/300_Longest_Increasing_Subsequence.cpp
+++ DP-SOLUTIONS/300_Longest_Increasing_Subsequence.cpp
@@ -125,4 +125,35 @@ public:
 
 
 
+// <-------------------------------------------------------------BINARY SEARCH------------------------------------------------------------------------------------->
+
+class Solution {
+public:
+// LIS + BINARY SEARCH 
+// TC: O(n*log(n))
+// SC: O(n)
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
+      for (int i = 1; i < n; i++)
+      {
+            if (nums[i] > temp.back())
+                  temp.push_back(nums[i]);
+            else
+            {
+                  // binary search  --> log(n)
+                  int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                  temp[ind] = nums[i];
+            }
+      }
+      return temp.size();  
+    }
+};
+
+
+
+
+
+
 
