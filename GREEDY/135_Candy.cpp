74d4417790c8937a7b562fcab5d5b385db454fc5
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-13 21:38:40 +0530

    Update 135_Candy.cpp

--- GREEDY/135_Candy.cpp
+++ GREEDY/135_Candy.cpp
@@ -34,3 +34,99 @@ public:
         return mincandies;
     }
 };
+
+
+
+// <--------------------------------------------------------------------SC: O(1) ------------------------------------------------------------------------------------>
+
+class Solution {
+public:
+    /*
+
+    APPROACH MOUNTAIN --> (UP, PEAK and DOWN)
+    TC: O(n)
+    SC: O(1)
+    ratings = {1,2,5,7,8,4,2,5,2,2}
+
+                        Here --> peak = 4 and down = 2
+
+                                                8
+                                               `  `
+                                              7    `
+                                             `      `
+                                            5        `      5
+                                           `          4    `  `
+                                          `            `  `    `
+                                         2              2       2 ` ` ` 2
+                                        `
+                                       1 
+
+                        Here -->  What If down = peak 
+                                       peak = 4  and down = 4  
+
+                                                 8
+                                               `  `
+                                              7    `
+                                             `      `
+                                            5        `      5
+                                           `          4    `  `
+                                          `            `  `    `
+                                         2              2       2 ` ` ` 2
+                                        `                `
+                                       1                  1
+                                                           `
+                                                            0
+            still holds good 
+
+            BUT,            
+                        Here -->  What If down > peak 
+                                       peak = 4  and down = 5  
+
+                                                 8
+                                               `  `
+                                              7    `
+                                             `      `
+                                            5        `      5
+                                           `          4    `  `
+                                          `            `  `    `
+                                         2              2       2 ` ` ` 2
+                                        `                `
+                                       1                  1
+                                                           `
+                                                            0
+                                                             `
+                                                              -1
+                 
+                    one more increments is needed  mincandies+1                               
+    */
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int mincandies = 1;  // base to banana padega for i = 0 ke liye
+        int peak = 0, up = 0, down = 0;
+        for(int i = 1; i<n; i++){
+                // ascending 
+                if(ratings[i]>ratings[i-1]) {
+                        peak=++up;
+                        down = 0;
+                        mincandies+=peak+1;
+                }
+                // stable horizontal level
+                else if(ratings[i]==ratings[i-1]) {
+                    up = 0;
+                    down = 0;
+                    peak=0;
+                    mincandies++;
+                }
+                // decreasing 
+                else {
+                    down++;
+                    up=0;
+                    if(down>peak) mincandies+=1;
+                    mincandies+=down;
+                }
+        }
+        return mincandies;
+    }
+};
+
+
