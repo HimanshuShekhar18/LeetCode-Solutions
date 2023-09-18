859d3d4af79cf6ef5e3e12f46175e834c5c32670
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-19 02:11:36 +0530

    Update 1444_Number_of_Ways_of_Cutting_a_Pizza.cpp

--- DP-SOLUTIONS/1444_Number_of_Ways_of_Cutting_a_Pizza.cpp
+++ DP-SOLUTIONS/1444_Number_of_Ways_of_Cutting_a_Pizza.cpp
@@ -50,4 +50,66 @@ public:
 // <-------------------------------------------------------------TABULATION------------------------------------------------------------------------------------------->
 
 
+class Solution {
+public:
+/* 
+TABULATION
+TC: O(rows*cols*k*(rows+cols))
+SC: O(rows*cols*k)
+*/
+    int rows;
+    int cols;
+    const int mod = 1000000007;
+    int dp[55][55][15];
+   
+    int ways(vector<string>& pizza, int k) {
+    rows = pizza.size();
+    cols = pizza[0].size();
+    vector<vector<int>> apples(rows+1,vector<int> (cols+1,0));
+    // storing how many apples available at any row and column
+    // starting from bottom-right
+    for(int i = rows-1; i>=0; i--){
+        for(int j = cols-1; j>=0; j--){
+            apples[i][j]=apples[i][j+1]+apples[i+1][j]-apples[i+1][j+1];
+            if(pizza[i][j]=='A') apples[i][j]++;
+        }
+    }
+    memset(dp,0,sizeof(dp));
+    // Base case
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (apples[i][j]>=1) {
+                    dp[i][j][1] = 1;
+                }
+                else dp[i][j][1] = 0;
+            }
+        }
+
+    // Fill up the DP table
+            for (int i = rows-1; i>=0; i--) {
+                for (int j = cols-1; j>=0; j--) {
+                    for (int people = 2; people <= k; people++) {
+                    long long ans = 0;
+        // Horizontal 
+            for(int hcut = i+1; hcut<rows; hcut++){
+                    int lower_slice = apples[hcut][j];
+                    int upper_slice = apples[i][j]-lower_slice;
+                    if(upper_slice >=1 && lower_slice >= people-1) ans+=dp[hcut][j][people-1];
+            }
+        // Vertical 
+            for(int vcut = j+1; vcut<cols; vcut++){
+                    int right_slice = apples[i][vcut];
+                    int left_slice = apples[i][j]-right_slice;
+                    if(left_slice >=1 && right_slice >= people-1) ans+=dp[i][vcut][people-1];
+            }
+                    dp[i][j][people] = ans%mod;
+                }
+            }
+        } 
+
+    return dp[0][0][k];    
+    }
+};
+
+
 
