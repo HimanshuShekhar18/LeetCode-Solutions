5ce6c34d9e8cc1163c6c0cceb016c82e17db7436
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-09-19 21:27:24 +0530

    Update 287_Find_the_Duplicate_Number.cpp

--- TWO POINTERS /287_Find_the_Duplicate_Number.cpp
+++ TWO POINTERS /287_Find_the_Duplicate_Number.cpp
@@ -13,3 +13,35 @@ public:
         return 0;
     }
 };
+
+
+// <------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+class Solution {
+public:
+// Otpimized Approach 2: Floyd Hare's - Tortoise Algorithm
+// TC: O(n)
+// SC: O(1)
+ 
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            cout << slow << " " << fast << endl;
+            if(slow==fast) break;
+        }
+        cout << endl;
+        int find = 0;
+        while(true){
+            slow=nums[slow];
+            find=nums[find];
+            cout << slow << " " << find << endl;
+            if(slow==find) return find;
+        }
+        return 0;
+    }
+};
+
