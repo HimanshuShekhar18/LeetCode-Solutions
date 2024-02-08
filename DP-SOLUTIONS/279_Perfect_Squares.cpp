fb4d1e77da8c0a0200274be1b75cfe211e914119
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-02-08 10:59:24 +0530

    Create 279_Perfect_Squares.cpp

---
+++ DP-SOLUTIONS/279_Perfect_Squares.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+/*
+Greedy(sabse pass wala perfect square) won't work 
+12 --> 9,3
+3 --> 1,2
+2--> 1,1
+Therefore, 12 = 9+1+1+1 i.e. 4 Perfect Squares
+*/
+
+/*
+DP Approach:
+                            12
+                        1 /  |\
+                         /  4| \ 9
+                        /    |  \
+                      11     8   3
+                   1/ | \9   |\
+                   / 4|  \  1| \4
+                  /   |   \  |  \
+                10   7    2  7  4
+                                |\
+                               1| \4
+                                |  \
+                                3   0
+    Path: 12->8->4->0
+
+*/
+
+// Recursion and Memoization
+// TC: O(n*sqrt(n))
+// SC: O(n)
+
+int dp[100005];
+
+    int perfect(int num){
+        if(num==0) return 0;
+        if(dp[num]!=-1) return dp[num];
+
+        int ans = INT_MAX;
+        for(int i = 1; i*i<=num; i++){
+            ans=min(ans,perfect(num-i*i)+1);
+        }
+        return dp[num]=ans;
+    }
+
+    int numSquares(int n) {
+        memset(dp,-1,sizeof(dp));
+        return perfect(n);
+    }
+};
+
+
