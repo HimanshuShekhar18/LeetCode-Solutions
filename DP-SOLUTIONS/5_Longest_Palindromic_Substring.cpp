85a302f0972c1950e80e0b9e5ed08230c49829b8
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-02-11 19:41:33 +0530

    Update 5_Longest_Palindromic_Substring.cpp

--- DP-SOLUTIONS/5_Longest_Palindromic_Substring.cpp
+++ DP-SOLUTIONS/5_Longest_Palindromic_Substring.cpp
@@ -134,3 +134,117 @@ bool solve(int i, int j, string &s){
 };
 
 
+
+// <-----------------------------------------------------------------MANACHER'S ALGORITHM---------------------------------------------------------------------->
+
+
+class Solution {
+public:
+
+/*
+LPS ( Longest Palindromic Substring )
+Print LPS ( Longest Palindromic Substring )
+
+Manacher's Algorithm 
+TC: O(n)
+SC: O(2*n+1)
+
+*/   
+
+struct manacher
+{
+      vector<int> p;
+      string t;
+      int start;
+      int maxLength;
+
+      // p[i] = defines how many length of palindrome we have at index i.
+      // Actual length of palindrome at p[i] is p[i] - 1, as We initially assigned 1 value to every p[i], So '#' characters also got length 1, which is why we have to subtract 1.
+      void run_manacher(string s)
+      {
+            // modified string "s"
+            // cout << s << endl;
+            int l = 1, r = 1;
+            int n = s.length();
+
+            p.assign(n, 1);
+            for (int i = 1; i < n; i++)
+            {
+              if(l+r-i>=0) p[i] = max(0, min(r - i, p[l + r - i]));
+
+                  // expanding outside
+                  while (i - p[i] >= 0 && i + p[i] < n && s[i - p[i]] == s[i + p[i]])
+                        p[i]++;
+
+                  // new bounding box or new longer palindrome
+                  if (i + p[i] > r)
+                  {
+                        l = i - p[i];
+                        r = i + p[i];
+
+                        if(p[i]-1>maxLength){
+                              start=l+2;
+                              maxLength=p[i]-1;
+                        }
+                  }
+            }
+
+            // for(auto v: p){
+            //       cout << v << " ";
+            // }
+            // cout << endl;
+      }
+
+      // Building the manacher
+      void build(string s)
+      {
+
+            for (auto v : s)
+            {
+                  t += string("#") + v;
+            }
+            t += string("#");
+
+            run_manacher(t);
+      }
+
+
+      string longestPalindromicSubstring(){
+            int n = t.size();
+            string ans = "";
+            while(maxLength){
+                  ans+=t[start];
+                  start+=2;
+                  maxLength--;
+            }
+         return ans;
+      }
+} m;
+
+    string longestPalindrome(string s) {
+      m.build(s);
+      return m.longestPalindromicSubstring();
+    }
+};
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
