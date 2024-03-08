d43b246e226ce31fa27ec006d22d4fe114dcf59a
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-03-08 08:03:41 +0530

    Create 169_Majority_Element.cpp

---
+++ ARRAY/169_Majority_Element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+// Boyer Moore Majority Voting Algorithm
+// TC: O(n)
+// SC: O(1)
+
+
+    int majorityElement(vector<int>& nums) {
+       int element ;
+       int count = 0;
+
+       for(int i = 0; i<nums.size(); i++){
+           if(count==0){
+               element = nums[i];
+               count=1;
+           }
+           else if(nums[i]==element) count++;
+           else count--;
+       }
+
+       return element;
+    }
+};
