5167b307198d5db5215b442c8110fa267b72a94f
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-03-13 08:35:55 +0530

    Update 741_Cherry_Pickup.cpp

--- DP-SOLUTIONS/741_Cherry_Pickup.cpp
+++ DP-SOLUTIONS/741_Cherry_Pickup.cpp
@@ -47,3 +47,86 @@ public:
         return max(0,solve(0,0,0,grid));
     }   
 };
+
+
+
+// <---------------------------------------------------------------BOTTOM-UP TABULATION-------------------------------------------------------------------------->
+
+
+
+class Solution {
+public:
+
+/*
+
+ 3D-DP 
+ BOTTOM - UP TABULATION
+
+ TC: O(n^3)
+ SC: O(n^3)
+
+ In Top-Down we started from Top-Leftmost cell,
+ In Bottom-Up, From Bottom-Right cell we  Starts and looks for Right and Down
+
+ (r1-0)+(c1-0)=(r2-0)+(c2-0)
+
+ r2 = r1+(c1-c2)
+
+ -->   -->
+ -->    ↓
+  ↓     ↓
+  ↓    -->
+
+*/
+    int n;
+    int dp[51][51][51];   
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        n=grid.size();
+        int r2;
+        memset(dp,0,sizeof(dp));
+
+        dp[n-1][n-1][n-1]=grid[n-1][n-1];
+
+        for(int r1 = n-1; r1>=0; r1--){
+            for(int c1 = n-1; c1>=0; c1--){
+                for(int c2 = n-1; c2>=0; c2--){
+
+                    r2 = r1+(c1-c2);
+
+                    if( r2<0 || r2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1 ) {
+                        dp[r1][c1][c2]=INT_MIN;
+                        continue;
+                    }
+                    
+                    int value = grid[r1][c1];
+                    if(c1!=c2) value+=grid[r2][c2];
+
+                    int cheerypick = INT_MIN;
+
+                    if(r1+1<n) cheerypick = max(cheerypick,dp[r1+1][c1][c2]);
+                    if(r1+1<n && c2+1<n) cheerypick = max(cheerypick,dp[r1+1][c1][c2+1]);
+                    if(c1+1<n) cheerypick = max(cheerypick,dp[r1][c1+1][c2]);
+                    if(c1+1<n && c2+1<n) cheerypick = max(cheerypick,dp[r1][c1+1][c2+1]);
+
+
+                    // no paths possible
+                    if(cheerypick==INT_MIN ) {
+                        // Neglecting the bottom right cell dp[n-1][n-1][n-1]=grid[n-1][n-1];
+                        if(r1!=n-1 || c1!=n-1 || c2!=n-1)  dp[r1][c1][c2] = INT_MIN; 
+                    }
+                    // path possible 
+                    else
+                    {
+                        value+=cheerypick;
+                        dp[r1][c1][c2]=value;
+                    }
+                }
+            }
+        }
+        return max(0,dp[0][0][0]);
+    }   
+};
+
+
+
