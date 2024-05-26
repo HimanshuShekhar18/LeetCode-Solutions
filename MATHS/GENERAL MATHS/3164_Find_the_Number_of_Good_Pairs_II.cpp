e8cb0f566c79709a307dfad66fbdb762fc489621
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-05-26 13:11:06 +0530

    Update 3164_Find_the_Number_of_Good_Pairs_II.cpp

--- MATHS/GENERAL MATHS/3164_Find_the_Number_of_Good_Pairs_II.cpp
+++ MATHS/GENERAL MATHS/3164_Find_the_Number_of_Good_Pairs_II.cpp
@@ -48,4 +48,75 @@ public:
 // <--------------------------------------------------------------BIT/FENWICK TREE---------------------------------------------------------------------------------------------------->
 
 
+class FenwickTree {
+public:
+    vector<long long> bit;
+    int n;
+    
+    FenwickTree(int size) : n(size) {
+        bit.resize(n + 1, 0);
+    }
+    
+    void update(int index, long long val) {
+        for (; index <= n; index += index & -index) {
+            bit[index] += val;
+        }
+    }
+    
+    long long query(int index) {
+        long long sum = 0;
+        for (; index > 0; index -= index & -index) {
+            sum += bit[index];
+        }
+        return sum;
+    }
+};
+
+class Solution {
+public:
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int maxVal = *max_element(nums1.begin(), nums1.end());
+        
+        // Multiply nums2 elements by k
+        for (int& num : nums2) {
+            num *= k;
+        }
+        
+        // Create a Fenwick Tree of appropriate size
+        FenwickTree fenwick(maxVal);
+        
+        // Populate the Fenwick Tree with counts from nums2
+        for (int num : nums2) {
+            if (num <= maxVal) {
+                fenwick.update(num, 1);
+            }
+        }
+        
+        long long count = 0;
+        
+        // For each number in nums1, find divisors and query counts
+        for (int num : nums1) {
+            for (int d = 1; d * d <= num; ++d) {
+                if (num % d == 0) {
+                    // Query count of d
+                    if (d <= maxVal) {
+                        count += fenwick.query(d) - fenwick.query(d - 1);
+                    }
+                    // If d is not the square root, query count of num / d
+                    int otherDiv = num / d;
+                    if (otherDiv != d && otherDiv <= maxVal) {
+                        count += fenwick.query(otherDiv) - fenwick.query(otherDiv - 1);
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+
 
