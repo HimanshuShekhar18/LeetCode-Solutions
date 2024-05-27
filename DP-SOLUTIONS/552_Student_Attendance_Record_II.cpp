47498982221b3abfb30bd7b4d1c1753835c2e851
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-05-27 13:05:20 +0530

    Update 552_Student_Attendance_Record_II.cpp

--- DP-SOLUTIONS/552_Student_Attendance_Record_II.cpp
+++ DP-SOLUTIONS/552_Student_Attendance_Record_II.cpp
@@ -139,3 +139,61 @@ public:
 
 
 
+// <--------------------------------------------------------SPACE OPTIMIZATION TABULATION---------------------------------------------------------------------------------------------->
+
+
+
+class Solution {
+public:
+    /*
+
+       SPACE OTIMIZED TABULATION
+
+       TC: O(n*2*3)
+       SC: O(1)
+
+    */
+    const int MOD = 1e9 + 7;
+    
+
+    int checkRecord(int n) {
+
+        
+        vector<vector<long long>> prev(3,vector<long long> (4,0));
+         vector<vector<long long>> curr(3,vector<long long> (4,0));
+        
+       
+        prev[0][0] = 1;
+
+        for (int index = 0; index < n; index++) {
+            
+            
+            for (int countabsent = 0; countabsent < 2; countabsent++) {
+                for (int countlate = 0; countlate <= 2; countlate++) {
+                        curr[countabsent][0] = (curr[countabsent][0] + prev[countabsent][countlate])%MOD;
+                        if (countabsent < 2) curr[countabsent+1][0]= (curr[countabsent+1][0] + prev[countabsent][countlate])%MOD;
+                        if (countlate  <= 2 ) curr[countabsent][countlate+1] = (curr[countabsent][countlate+1] + prev[countabsent][countlate])%MOD;
+                    }
+                }
+            prev = curr;
+            // for every new length new curr vector is required in which all values are ZERO
+            for(int i = 0; i<3; i++){
+                for(int j = 0; j<4; j++){
+                    curr[i][j]=0;
+                }
+            }
+        }
+
+        long long count = 0;
+
+            for (int countabsent = 0; countabsent < 2; countabsent++) {
+                for (int countlate = 0; countlate <= 2; countlate++) {
+                        count+=prev[countabsent][countlate];
+                    }
+            }
+
+        return count%MOD;
+    }
+};
+
+
