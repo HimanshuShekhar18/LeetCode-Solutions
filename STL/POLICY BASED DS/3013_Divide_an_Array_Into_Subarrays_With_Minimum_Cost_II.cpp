ab2941afadcccedd294175331c73307a156bfb79
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-08-23 15:35:05 +0530

    Update 3013_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_II.cpp

--- STL/POLICY BASED DS/3013_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_II.cpp
+++ STL/POLICY BASED DS/3013_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_II.cpp
@@ -54,3 +54,91 @@ public:
 };
 
 
+
+// <----------------------------------------------------------------TWO MULTISETS--------------------------------------------------------------------------------------------------------->
+
+/*
+Two Multiset and Sliding Window
+TC: O(n*log(k))
+SC: O(n)
+*/
+
+class Solution {
+public:
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+
+        long long ans = nums[0];
+
+        int n = nums.size();
+
+        multiset<int> st1;
+        multiset<int> st2;
+
+        // for the first window Add the first 'dist + 1' elements to the left multiset
+        for (int i = 1; i <= dist + 1; i++) {
+            st1.insert(nums[i]);
+        }
+
+        // for smaller 'k-1' values
+        auto it = st1.begin();
+        for (int i = 0; i < k - 1; i++) {
+            ans += *(it);
+            ++it;
+        }
+
+        // Deleting access elements in st1 and saving those in st2
+        while(st1.size()>k-1){
+            st2.insert(*(st1.rbegin()));
+            st1.erase(st1.find(*(st1.rbegin())));
+        }
+
+   
+        long long sum = ans;
+
+        // for the other windows
+        for (int i = dist + 1; i < n - 1; i++) {
+
+            int to_delete = nums[i-dist];
+
+            if(st1.find(to_delete)!=st1.end()){
+                sum-=to_delete;
+                st1.erase(st1.find(to_delete));
+            }
+            else{
+                st2.erase(st2.find(to_delete));
+            }
+
+            int to_add = nums[i+1];
+
+            if( to_add <= *st1.rbegin()){
+                sum+=to_add;
+                st1.insert(to_add);
+            }
+            else{
+                st2.insert(to_add);
+            }
+
+            // if st1 contains less elements than k-1
+            while(st1.size()<k-1){
+                
+                sum+= *st2.begin();
+                st1.insert(*(st2.begin()));
+                st2.erase(st2.find(*st2.begin()));
+            }
+
+            // if st1 contains more elements than k-1
+            while(st1.size()>k-1){
+                sum-=*(st1.rbegin());
+                st2.insert(*(st1.rbegin()));
+                st1.erase(st1.find(*st1.rbegin()));
+            }
+        
+            ans = min(ans, sum);
+        }
+        
+        return ans;
+    }
+};
+
+
+
